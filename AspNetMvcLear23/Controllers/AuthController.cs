97994d0ff805ee5f760a2505fc294a7a86964494
lear23 <130910243+lear23@users.noreply.github.com>
2024-08-06 10:52:14 +0200

    Add project files.

---
+++ AspNetMvcLear23/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMvcLear23.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetMvcLear23/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMvcLear23.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
